53664e4889c32a6c6c8c9e6c32d1fa87492e2ea6
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-07-05 16:18:20 +0530

    updated

---
+++ DynamicNumberArray/DynamicNumberArray/DynamicNumberArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicNumberArray
+{
+        public class DynamicNumberArray
+        {
+            private int[] buffer;
+            public DynamicNumberArray()
+            {
+                buffer = new int[5];
+            }
+            public DynamicNumberArray(int initialSize)
+            {
+                buffer = new int[initialSize];
+            }
+            public int this[int index]
+            {
+                set
+                {
+                    Array.Resize(ref buffer, index + 5);
+                    buffer[index] = value;
+                }
+                get
+                {
+                    return buffer[index];
+                }
+            }
+        }
+    }
---
+++ DynamicNumberArray/DynamicNumberArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicNumberArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicNumberArray _dynamic = new DynamicNumberArray(5);
+            _dynamic[0] = 10;
+            _dynamic[1] = 20;
+            _dynamic[2] = 30;
+            _dynamic[3] = 40;
+            _dynamic[4] = 50;
+            _dynamic[5] = 60;
+
+            int value = _dynamic[5];
+            Console.WriteLine(value);
+           
+        }
+    }
+
+}
